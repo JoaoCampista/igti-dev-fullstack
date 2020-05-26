263d5ecf589d6b92846d2e2e77feaa01c54fd743
joaocampista <joaocampista.jc@gmail.com>
2020-05-25 21:23:56 -0300

    adicionado aula11

---
+++ modulo-1/aula-11-javascript-moderno-manipulacao-arrays/js/script.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', () => {
+  const nameEmailArray = people.results.map((person) => {
+    return {
+      name: person.name,
+      email: person.email,
+    };
+  });
+
+  console.log(nameEmailArray);
+});
