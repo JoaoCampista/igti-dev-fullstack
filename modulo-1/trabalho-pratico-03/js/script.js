d1866745d5cc61ff1d926960e1a676824994cb4f
joaocampista <joaocampista.jc@gmail.com>
2020-05-28 02:19:44 -0300

    atualizado trabalho pratico 3

--- modulo-1/trabalho-pratico-03/js/script.js
+++ modulo-1/trabalho-pratico-03/js/script.js
@@ -1,34 +1,40 @@
 // Estado da aplicação (state)
 
 let usersFoundList = null;
+let usersFavoriteList = null;
 let userSearch = null;
+let textUsersFound = null;
+let textInitial = null;
+let dataContainer = null;
+let numberFormat = null;
 
 let allUsers = [];
 let filteredUsers = [];
+let favoriteUsers = [];
 
 let countMale = 0;
 let countFemale = 0;
 let sumAge = 0;
 let meanAge = 0;
-
 let totalUsersFound = 0;
 
-let numberFormat = null;
-
 window.addEventListener('load', () => {
-  usersFoundList = document.querySelector('#usersFoundList');
   userSearch = document.querySelector('#userSearch');
-  totalUsersFound = document.querySelector('#usersFound');
+  dataContainer = document.querySelector('#dataContainer');
 
-  countMale = document.querySelector('#countMale');
-  countFemale = document.querySelector('#countFemale');
-  sumAge = document.querySelector('#sumAge');
-  meanAge = document.querySelector('#meanAge');
-
-  userSearch.addEventListener('keyup', () => render());
-
+  userSearch.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter' && event.target.value.trim() !== '') {
+      console.log(favoriteUsers);
+      filterUserList(userSearch.value.toLowerCase());
+      render();
+      renderUserFavoriteList();
+    } else {
+      if (event.key === 'Enter' && userSearch.value.trim() === '') {
+        render();
+      }
+    }
+  });
   numberFormat = Intl.NumberFormat('pt-BR');
-
   fetchUsers();
 });
 
@@ -50,61 +56,191 @@ async function fetchUsers() {
       gender,
     };
   });
-  render();
+  //render();
 }
 
 function render() {
-  filterUserList(userSearch.value.toLowerCase());
   renderUserList();
-  renderUserCount();
+  //renderUserFavoriteList();
   renderStats();
+  handleUsersButtons();
 }
 
 function filterUserList(nameToFilter) {
-  console.log(nameToFilter);
   filteredUsers = allUsers.filter((user) => {
     return user.nameFull.toLowerCase().includes(nameToFilter);
   });
+
+  if (favoriteUsers.length !== 0) {
+    favoriteUsers.forEach((userFavorite) => {
+      filteredUsers = filteredUsers.filter(
+        (user) => user.id !== userFavorite.id
+      );
+    });
+  }
 }
 
 function renderUserList() {
-  let usersHTML = '<div>';
-
-  filteredUsers.forEach((user) => {
-    const { id, nameFirst, nameLast, avatar, age } = user; // destructuring
-
-    const userHTML = `
-    <div class='user' id='${id}'>
-      <div>
-        <img src="${avatar}" alt="${nameFirst}-${nameLast}"/>
+  if (userSearch.value.trim() !== '') {
+    const dataContainerHTML = `
+      <div class="w-50 border rounded-lg bg-white mr-3">
+        <h4 class="d-flex justify-content-center mb-3" id="textInitial"><span class="mr-2" id="usersFound"></span><span id="textUsersFound"></span></h4>
+        <div id="usersFoundList"></div>
       </div>
-      <div>
-        <p><span>${nameFirst} ${nameLast}</span>, <span>${age}</span> anos</p>
+
+      <div class="w-50 d-flex flex-column">
+        <div class="border rounded-lg bg-white">
+          <h4 class="d-flex justify-content-center mb-3">informações</h4>
+          <div class="d-flex flex-column ml-3 mb-3">
+            <p class="my-0">sexo masculino: <span id="countMale"></span></p>
+            <p class="my-0">sexo feminino: <span id="countFemale"></span></p>
+            <p class="my-0">soma das idades: <span id="sumAge"></span></p>
+            <p class="my-0">média das idades: <span id="meanAge"></span></p>
+          </div>
+        </div>
+        <div id = "containerFavorites"></div>
       </div>
+    `;
+    dataContainer.innerHTML = dataContainerHTML;
+
+    usersFoundList = document.querySelector('#usersFoundList');
+    containerFavorites = document.querySelector('#containerFavorites');
+
+    totalUsersFound = document.querySelector('#usersFound');
+    textUsersFound = document.querySelector('#textUsersFound');
+
+    textInitial = document.querySelector('#textInitial');
+
+    countMale = document.querySelector('#countMale');
+    countFemale = document.querySelector('#countFemale');
+    sumAge = document.querySelector('#sumAge');
+    meanAge = document.querySelector('#meanAge');
+
+    let usersHTML = `<div class='d-flex justify-content-start flex-column'>`;
+
+    filteredUsers.forEach((user) => {
+      const { id, nameFirst, nameLast, avatar, age } = user; // destructuring
+      const userHTML = `
+    <div class='d-flex flex-row align-items-center bd-highlight mb-3 ml-3 id='${id}'>
+      <div class='d-flex flex-row align-items-center w-75'>
+        <div>
+          <img src="${avatar}" alt="${nameFirst}-${nameLast}" class="rounded-circle mr-2"/>
+        </div>
+        <div>
+          <span>${nameFirst} ${nameLast}</span>, <span>${age}</span> anos
+        </div>
+      </div>
+        <div class='w-25 d-flex justify-content-end mr-3'>
+          <button type="button" id="${id}"class="btn btn-dark">+</button>
+        </div>
     </div>
     `;
+      usersHTML += userHTML;
+    });
+    usersHTML += '</div>'; //fechando a div
+    usersFoundList.innerHTML = usersHTML;
+    renderUserCount();
+  } else dataContainer.innerHTML = '';
+}
 
-    usersHTML += userHTML;
-  });
+function renderUserFavoriteList() {
+  if (favoriteUsers.length !== 0) {
+    console.log('entreeei');
+    containerFavorites.innerHTML = `
+    <div class="border rounded-lg bg-white mt-3 mb-3">
+      <h4 class="d-flex justify-content-center">favoritos</h4>
+      <div class="d-flex flex-column" id="usersFavoriteList"></div>
+    </div>
+    `;
+    usersFavoriteList = document.querySelector('#usersFavoriteList');
 
-  usersHTML += '</div>'; //fechando a div
+    let favoritesHTML = `<div class='d-flex justify-content-start flex-column'>`;
 
-  usersFoundList.innerHTML = usersHTML;
+    favoriteUsers.forEach((user) => {
+      const { id, nameFirst, nameLast, avatar, age } = user; // destructuring
+      const favoriteHTML = `
+            <div class='d-flex flex-row align-items-center bd-highlight mb-3 ml-3 id='${id}'>
+              <div class='d-flex flex-row align-items-center w-75'>
+                <div>
+                  <img src="${avatar}" alt="${nameFirst}-${nameLast}" class="rounded-circle mr-2"/>
+                </div>
+                <div>
+                  <span>${nameFirst} ${nameLast}</span>, <span>${age}</span> anos
+                </div>
+              </div>
+                <div class='w-25 d-flex justify-content-end mr-3'>
+                  <button type="button" id="${id}"class="btn btn-dark">-</button>
+                </div>
+            </div>
+            `;
+      favoritesHTML += favoriteHTML;
+    });
+    favoritesHTML += '</div>'; //fechando a div
+    usersFavoriteList.innerHTML = favoritesHTML;
+  }
 }
 
 function renderUserCount() {
   totalUsersFound.innerHTML = filteredUsers.length;
+
+  if (filteredUsers.length === 1) {
+    console.log(filteredUsers.length);
+    textUsersFound.innerHTML = 'usuário encontrado';
+  } else {
+    textUsersFound.innerHTML = 'usuários encontrados';
+  }
 }
 
 function renderStats() {
-  //prettier-ignore
-  countMale.innerHTML = filteredUsers.filter((user) => user.gender === 'male').length;
-  //prettier-ignore
-  countFemale.innerHTML = filteredUsers.filter((user) => user.gender === 'female').length;
+  countMale.innerHTML = filteredUsers.filter(
+    (user) => user.gender === 'male'
+  ).length;
+  countFemale.innerHTML = filteredUsers.filter(
+    (user) => user.gender === 'female'
+  ).length;
 
   sumAge.innerHTML = filteredUsers.reduce((accumulator, current) => {
     return accumulator + current.age;
   }, 0);
 
   meanAge.innerHTML = sumAge.innerHTML / filteredUsers.length;
+}
+
+function handleUsersButtons() {
+  const userButtons = Array.from(usersFoundList.querySelectorAll('.btn'));
+
+  userButtons.forEach((button) => {
+    button.addEventListener('click', () => addToFavorites(button.id));
+  });
+}
+
+function handleFavoriteButtons() {
+  // prettier-ignore
+  const favoriteButtons = Array.from(usersFavoriteList.querySelectorAll('.btn'));
+
+  favoriteButtons.forEach((button) => {
+    button.addEventListener('click', () => removeFromFavorites(button.id));
+  });
+}
+
+function addToFavorites(id) {
+  const userToAdd = filteredUsers.find((user) => user.id === id);
+
+  favoriteUsers = [...favoriteUsers, userToAdd];
+  filteredUsers = filteredUsers.filter((user) => user.id !== id);
+
+  render();
+  renderUserFavoriteList();
+  handleFavoriteButtons();
+}
+
+function removeFromFavorites(id) {
+  const userToRemove = favoriteUsers.find((user) => user.id === id);
+
+  filteredUsers = [...filteredUsers, userToRemove];
+  favoriteUsers = favoriteUsers.filter((user) => user.id !== id);
+  console.log(favoriteUsers);
+  render();
+  renderUserFavoriteList();
+  handleFavoriteButtons();
 }
